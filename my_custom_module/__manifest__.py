969421a353208175ce46656a1aca18ff178df87a
Muhammad Awais <awaisqureshidev01@gmail.com>
2026-06-19 01:21:28 +0500

    Clean working module for Partner Ledger Tax Column

---
+++ my_custom_module/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Partner Ledger Tax Column',
+    'version': '1.0',
+    'summary': 'Adds a tax column to the Partner Ledger report',
+    'depends': ['account', 'account_reports'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
