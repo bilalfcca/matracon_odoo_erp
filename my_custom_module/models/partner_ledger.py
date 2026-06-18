bc04756eb881b16ce97d7bfad13cec2173aef0a0
Muhammad Awais <awaisqureshidev01@gmail.com>
2026-06-19 00:56:16 +0500

    Fixed model name for Odoo 19: account.partner.ledger.report

--- my_custom_module/models/partner_ledger.py
+++ my_custom_module/models/partner_ledger.py
@@ -1,29 +1,23 @@
-from odoo import models, api
+from odoo import models
 
-class PartnerLedgerReportHandler(models.AbstractModel):
-    _inherit = 'account.partner.ledger.report.handler'
+class PartnerLedgerReport(models.AbstractModel):
+    _inherit = 'account.partner.ledger.report'
 
     def _get_report_line_partners(self, options, partners):
-        # Get the base lines from parent
         lines = super()._get_report_line_partners(options, partners)
-
-        # Add tax amount to each line
+        
         for line in lines:
-            # Get the account move line
             line_id = line.get('id')
             if line_id:
                 move_line = self.env['account.move.line'].browse(line_id)
                 if move_line.exists():
-                    # Get tax amount
                     if move_line.tax_line_id:
-                        # This is a tax line itself
                         line['tax_amount'] = abs(move_line.balance)
                     else:
-                        # Check if this move has tax lines
                         tax_amount = 0.0
                         for line2 in move_line.move_id.line_ids:
                             if line2.tax_line_id and line2.account_id == move_line.account_id:
                                 tax_amount += abs(line2.balance)
                         line['tax_amount'] = tax_amount
-
+        
         return lines
